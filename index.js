4d682882211d254f466328e06128f1ff342f0c75
Alicelai <alicev871203@gmail.com>
2020-07-28 15:52:09 +0800

    mnn

--- index.js
+++ index.js
@@ -36,3 +36,22 @@ function f3() {
         z.style.display = "none";
     }
 }
+
+
+function web() {
+    var x = document.getElementById("web");
+    var y = document.getElementById("graphic");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    }
+};
+
+function graphic() {
+    var x = document.getElementById("web");
+    var y = document.getElementById("graphic");
+    if (y.style.display === "none") {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+};
